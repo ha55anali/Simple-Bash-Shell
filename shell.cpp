eee07fb109ef8d7b9dcfef9f99dc657e42dc5daf
Hassan Ali <phelenon@gmail.com>
2020-02-14 00:19:53 +0500

    Implemented parse command function
    takes input from console and parses into struct command_t

--- shell.cpp
+++ shell.cpp
@@ -9,13 +9,13 @@ using namespace std;
 void printPrompt() { cout << endl << "myBamsh>>"; }
 
 void readCommand(char *&buffer) {
-  buffer = new char[500];
+  buffer = new char[MAX_COMMAND];
 
-  fgets(buffer, sizeof(buffer), stdin);
+  fgets(buffer, MAX_COMMAND, stdin);
 }
 
 int parsePath(char *dirs[]) {
-  //get system paths
+  // get system paths
   char *pathEnvVar;
   char *thePath;
   for (int i = 0; i < MAX_PATHS; i++)
@@ -27,13 +27,13 @@ int parsePath(char *dirs[]) {
   // store path list to dirs
   int count = 0;
   int dirCount = 0;
-  char buff[1000];
+  char buff[MAX_PATH_LEN];
   int buffCount = 0;
   while (true) {
-	//if end of line
+	// if end of line
 	if (thePath[count] == '\0')
 	  break;
-	//delimiter :
+	// delimiter :
 	else if (thePath[count] == ':') {
 	  dirs[dirCount] = new char[strlen(buff) + 1];
 	  strcpy(dirs[dirCount], buff);
@@ -51,7 +51,7 @@ int parsePath(char *dirs[]) {
   }
 
   // get current directory
-  char cwd[MAX_PATHS];
+  char cwd[MAX_PATH_LEN];
   if (getcwd(cwd, sizeof(cwd)) != NULL) {
 	dirs[dirCount] = new char[strlen(cwd) + 1];
 	strcpy(dirs[dirCount], cwd);
@@ -60,3 +60,59 @@ int parsePath(char *dirs[]) {
   return 1;
 }
 
+void parseCommand(char *commandLine, command_t &command) {
+  // get command name
+  char buff[MAX_COMMAND] = "";
+  int buffCount=0;
+  int count = 0;
+  while (1) {
+	if (commandLine[count] == ' ' || commandLine[count] == '\0') {
+	  command.name = new char[strlen(buff) + 1];
+	  strcpy(command.name, buff);
+
+	  buffCount = 0;
+	  buff[0] = '\0';
+	  ++count;
+
+	  break;
+	} else {
+	  buff[buffCount] = commandLine[count];
+	  buff[buffCount+1] = '\0';
+	  ++buffCount;
+	  ++count;
+	  
+
+	}
+  }
+
+  // get command line arguments
+  command.argc = 0;
+  while (1) {
+	if (commandLine[count] == '\0') {
+	  command.argv[command.argc] = new char[strlen(buff) + 1];
+	  strcpy(command.argv[command.argc], buff);
+
+	  buffCount = 0;
+	  buff[0] = '\0';
+	  ++count;
+	  ++(command.argc);
+
+	  break;
+	}
+	if (commandLine[count] == ' ') {
+	  command.argv[command.argc] = new char[strlen(buff) + 1];
+	  strcpy(command.argv[command.argc], buff);
+
+	  buffCount = 0;
+	  buff[0] = '\0';
+	  ++count;
+	  ++(command.argc);
+
+	} else {
+	  buff[buffCount] = commandLine[count];
+	  ++buffCount;
+	  ++count;
+	  buff[buffCount] = '\0';
+	}
+  }
+}
--- shell.h
+++ shell.h
@@ -1,17 +1,29 @@
 #pragma once
 
-#define LINE_LEN     80
-#define MAX_ARGS     64
-#define MAX_ARG_LEN  16
-#define MAX_PATHS    64
+#define LINE_LEN 80
+#define MAX_ARGS 64
+#define MAX_ARG_LEN 16
+#define MAX_PATHS 64
 #define MAX_PATH_LEN 96
-#define WHITESPACE   " .,\t\n"
+#define WHITESPACE " .,\t\n"
+#define MAX_COMMAND 300
 
-//print prompt for terminal
+struct command_t {
+  char *name;
+  int argc;
+  char **argv;
+
+  command_t() { argv = new char *[MAX_ARGS]; }
+};
+
+// print prompt for terminal
 void printPrompt();
 
-//takes input from the terminal
-void readCommand(char*& buffer);
+// takes input from the terminal
+void readCommand(char *&buffer);
 
-//returns array of directories to search
+// returns array of directories to search
 int parsePath(char *dirs[]);
+
+// parses string into command_t
+void parseCommand(char *commandLine, command_t &command);
